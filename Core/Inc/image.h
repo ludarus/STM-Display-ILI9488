13329883533488ac4048481b5c55f7bad462c9b2
ludarus <burgermanjoe48@gmail.com>
2026-06-18 12:35:08 -0400

    image decoding, structs for images, init commands for displaying images

---
+++ Core/Inc/image.h
@@ -0,0 +1,18 @@
+/*
+ * image.h
+ *
+ *  Created on: Jun 18, 2026
+ *      Author: Luke Fadel
+ */
+
+#ifndef INC_IMAGE_H_
+#define INC_IMAGE_H_
+
+typedef struct {
+	const uint16_t width;
+	const uint16_t height;
+	const uint8_t *data;
+	const uint32_t size;
+} Image_t;
+
+#endif /* INC_IMAGE_H_ */
--- Core/Src/display.c
+++ Core/Src/display.c
@@ -10,6 +10,20 @@
 
 #include "main.h"
 #include <stdbool.h>
+#include "image.h"
+#include <stdbool.h>
+
+// bit packing to save memory
+static uint8_t screenCopy[((480 * 320) + 7) / 8];
+
+// macros to set and access bits in the array
+// sets pixel to 1
+#define SET_PIXEL(array, bit) ((array)[(bit) / 8] |= (1u <<((bit) % 8))) // returns void
+// sets pixel to 0
+#define CLR_PIXEL(arr, bit) ((arr)[(bit)/8] &= ~(1u << ((bit) % 8)))
+// shifting byte to desired bit and masking off the rest of the bit
+#define GET_PIXEL(array, bit) (((array)[(bit) / 8] >> ((bit) % 8)) & 1u) // returns 0u or 1u
+#define OR_PIXEL(arr, bit, val) ((arr)[(bit)/8] |= ((!!(val)) << ((bit) % 8)))
 
 // LCD hardware Reset, active low
 void DISPLAY_RESET(void) {
@@ -54,7 +68,7 @@ void DISPLAY_CMD(SPI_HandleTypeDef *spi, uint8_t cmd) {
 }
 
 //sends data to lcd
-void DISPLAY_DATA(SPI_HandleTypeDef *spi, uint8_t cmd, uint16_t size) {
+void DISPLAY_DATA(SPI_HandleTypeDef *spi, uint8_t *data, uint16_t size) {
 	//setting DC pin to command mode (low)
 	HAL_GPIO_WritePin(DISPLAY_DC_GPIO_Port, DISPLAY_DC_Pin, GPIO_PIN_RESET);
 
@@ -62,7 +76,7 @@ void DISPLAY_DATA(SPI_HandleTypeDef *spi, uint8_t cmd, uint16_t size) {
 	DISPLAY_SELECT();
 
 	//using SPI to transmit data
-	HAL_SPI_Transmit(spi, &cmd, size, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(spi, data, size, HAL_MAX_DELAY);
 
 	//deselecting SPI device
 	DISPLAY_DESELECT();
@@ -95,11 +109,13 @@ void DISPLAY_INIT(SPI_HandleTypeDef *spi) {
 	DISPLAY_CMD(spi, 0x3A);
 	// Lowest available is 16bit/pixel
 	// 01010101
-	DISPLAY_DATA(spi, 0x55, 1);
+	uint8_t colmod = 0x55;
+	DISPLAY_DATA(spi, &colmod, 1);
 
 	// Column inversion for display longevity
 	DISPLAY_CMD(spi, 0xB4);
-	DISPLAY_DATA(spi, 0x01, 1);
+	uint8_t inversion = 0x01;
+	DISPLAY_DATA(spi, &inversion, 1);
 
 	// NOTE: if unlock was required before, a lock is required now
 
@@ -109,6 +125,60 @@ void DISPLAY_INIT(SPI_HandleTypeDef *spi) {
 }
 
 void DISPLAY_WRITE(SPI_HandleTypeDef *spi, uint16_t x, uint16_t y,
-		uint8_t *image, bool overWrite) {
+		Image_t *image, bool overWrite) {
+	// set column address command
+	DISPLAY_CMD(spi, 0x2A);
+	// parameters: starting col MSB, starting col LSB, ending col MSB, ending col LSB
+	uint8_t caset[] = { (uint8_t) (x >> 8), (uint8_t) x, (uint8_t) ((uint8_t) (x
+			+ image->width) >> 8), (uint8_t) (x + image->width) };
+	DISPLAY_DATA(spi, caset, 4);
+
+	// set row address command
+	DISPLAY_CMD(spi, 0x2B);
+	// parameters: starting row MSB, starting row LSB, ending row MSB, ending row LSB
+	uint8_t raset[] = { (uint8_t) (y >> 8), (uint8_t) y, (uint8_t) ((uint8_t) (y
+			+ image->height) >> 8), (uint8_t) (y + image->height) };
+	DISPLAY_DATA(spi, raset, 4);
+
+	// decompressing image as a bit array. Storing statically as max size on heap to prevent ram overflows
+	static uint8_t dcompImage[((320 * 480) + 7) / 8];
+	uint32_t count = 0;
+	for (uint32_t i = 0; i < image->size; i++) {
+		for (uint8_t j = 0; j < image->data[i]; j++) {
+			if (i % 2) {
+				//pixel is high
+				SET_PIXEL(dcompImage, count);
+			} else {
+				//pixel is low
+				CLR_PIXEL(dcompImage, count);
+			}
+			count++;
+		}
+	}
+
+	// modifying the cloned buffer
+	for (uint16_t h = 0; h < image->height; h++) {
+		for (uint16_t w = 0; w < image->width; w++) {
+			uint32_t globalpos = 480 * (y + h) + x + w;
+			uint32_t localpos = (image->width * h) + w;
+			if (overWrite) {
+				if (GET_PIXEL(dcompImage, localpos)) {
+					SET_PIXEL(screenCopy, globalpos);
+				} else {
+					CLR_PIXEL(screenCopy, globalpos);
+				}
+			} else {
+				OR_PIXEL(screenCopy, globalpos, GET_PIXEL(dcompImage,localpos));
+			}
+		}
+	}
+
+//	// write data command
+//	DISPLAY_CMD(spi, 0x2C);
+//
+//	// setting to data mode
+//	HAL_GPIO_WritePin(DISPLAY_DC_GPIO_Port, DISPLAY_DC_Pin, GPIO_PIN_RESET);
+
+	// write to display code here
 
 }
